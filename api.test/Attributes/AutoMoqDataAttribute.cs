d17ead39c7fc2d6f0dce5e742d9651694c07972e
vladzavorotny <vlad.zavorotny.vz@gmail.com>
2021-08-02 12:59:27 +0300

    Drop files from .gitignore

---
+++ api.test/Attributes/AutoMoqDataAttribute.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.test.Attributes
+{
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute()
+            :base(() => new Fixture().Customize(new AutoMoqCustomization()))
+        {
+
+        }
+    }
+}
--- api.test/CartControllerTest.cs
+++ api.test/CartControllerTest.cs
@@ -1,75 +1,99 @@
 // CartControllerTest.cs
-
-using System;
 using Services;
 using Moq;
-using NUnit.Framework;
 using api.Controllers;
 using System.Linq;
 using System.Collections.Generic;
+using Xunit;
+using api.test.Attributes;
+using AutoFixture.Xunit2;
+using FluentAssertions;
 
 namespace test
 {
-  public class Tests
-  {
-      private CartController controller;
-      private Mock<IPaymentService> paymentServiceMock;
-      private Mock<ICartService> cartServiceMock;
+    public class Tests
+    {
+        /*private CartController controller;
+        private Mock<IPaymentService> paymentServiceMock;
+        private Mock<ICartService> cartServiceMock;
 
-      private Mock<IShipmentService> shipmentServiceMock;
-      private Mock<ICard> cardMock;
-      private Mock<IAddressInfo> addressInfoMock;
-      private List<CartItem> items;
+        private Mock<IShipmentService> shipmentServiceMock;
+        private Mock<ICard> cardMock;
+        private Mock<IAddressInfo> addressInfoMock;
+        private List<CartItem> items;*/
 
-      [SetUp]
-      public void Setup()
-      {
-          
-          cartServiceMock = new Mock<ICartService>();
-          paymentServiceMock = new Mock<IPaymentService>();
-          shipmentServiceMock = new Mock<IShipmentService>();
+        /*public void Setup()
+        { 
+            cartServiceMock = new Mock<ICartService>();
+            paymentServiceMock = new Mock<IPaymentService>();
+            shipmentServiceMock = new Mock<IShipmentService>();
 
-          cardMock = new Mock<ICard>();
-          addressInfoMock = new Mock<IAddressInfo>();
+            cardMock = new Mock<ICard>();
+            addressInfoMock = new Mock<IAddressInfo>();
 
-          var cartItemMock = new Mock<CartItem>();
-          cartItemMock.Setup(item => item.Price).Returns(10);
+            var cartItemMock = new Mock<CartItem>();
+            cartItemMock.Setup(item => item.Price).Returns(10);
 
-          items = new List<CartItem>()
-          {
+            items = new List<CartItem>()
+            {
               cartItemMock.Object
-          };
+            };
 
-          cartServiceMock.Setup(c => c.Items()).Returns(items.AsEnumerable());
+            cartServiceMock.Setup(c => c.Items()).Returns(items.AsEnumerable());
 
-          controller = new CartController(cartServiceMock.Object, paymentServiceMock.Object, shipmentServiceMock.Object);
-      }
+            controller = new CartController(cartServiceMock.Object, paymentServiceMock.Object, shipmentServiceMock.Object);
+        }*/
 
-      [Test]
-      public void ShouldReturnCharged()
-      {
-          paymentServiceMock.Setup(p => p.Charge(It.IsAny<double>(), cardMock.Object)).Returns(true);
+        [Theory]
+        [AutoMoqData]
+        public void ShouldReturnCharged(
+              [Frozen]Mock<ICartService> cartServiceMock
+            , [Frozen]Mock<IPaymentService> paymentServiceMock
+            , [Frozen]Mock<IShipmentService> shipmentServiceMock
+            , Mock<ICard> cardMock
+            , Mock<IAddressInfo> addressInfoMock
+            , [Frozen] Mock<CartItem> cartItemMock
+            , List<CartItem> items
+            , CartController controller
+          )
+        {
+            //arrange
+            cartItemMock.Setup(item => item.Price).Returns(10);
+            cartServiceMock.Setup(c => c.Items()).Returns(items.AsEnumerable());
+            paymentServiceMock.Setup(p => p.Charge(It.IsAny<double>(), cardMock.Object)).Returns(true);
 
-          // act
-          var result = controller.CheckOut(cardMock.Object, addressInfoMock.Object);
+            // act
+            var result = controller.CheckOut(cardMock.Object, addressInfoMock.Object);
 
-          // assert
-          shipmentServiceMock.Verify(s => s.Ship(addressInfoMock.Object, items.AsEnumerable()), Times.Once());
+            // assert
+            shipmentServiceMock.Verify(s => s.Ship(addressInfoMock.Object, items.AsEnumerable()), Times.Once());
+            result.Should().Be("charged");
+        }
 
-          Assert.AreEqual("charged", result);
-      }
+        [Theory]
+        [AutoMoqData]
+        public void ShouldReturnNotCharged(
+              [Frozen] Mock<ICartService> cartServiceMock
+            , [Frozen] Mock<IPaymentService> paymentServiceMock
+            , [Frozen] Mock<IShipmentService> shipmentServiceMock
+            , Mock<ICard> cardMock
+            , Mock<IAddressInfo> addressInfoMock
+            , [Frozen] Mock<CartItem> cartItemMock
+            , List<CartItem> items
+            , CartController controller
+            ) 
+        {
+            //arrange
+            cartItemMock.Setup(item => item.Price).Returns(10);
+            cartServiceMock.Setup(c => c.Items()).Returns(items.AsEnumerable());
+            paymentServiceMock.Setup(p => p.Charge(It.IsAny<double>(), cardMock.Object)).Returns(false);
 
-      [Test]
-      public void ShouldReturnNotCharged() 
-      {
-          paymentServiceMock.Setup(p => p.Charge(It.IsAny<double>(), cardMock.Object)).Returns(false);
+            // act
+            var result = controller.CheckOut(cardMock.Object, addressInfoMock.Object);
 
-          // act
-          var result = controller.CheckOut(cardMock.Object, addressInfoMock.Object);
-
-          // assert
-          shipmentServiceMock.Verify(s => s.Ship(addressInfoMock.Object, items.AsEnumerable()), Times.Never());
-          Assert.AreEqual("not charged", result);
-      }
-  }
+            // assert
+            shipmentServiceMock.Verify(s => s.Ship(addressInfoMock.Object, items.AsEnumerable()), Times.Never());
+            result.Should().Be("not charged");
+        }
+    }
 }
